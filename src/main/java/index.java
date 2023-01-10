68f22d1fe46f90de32833872be4643e4d4e71d97
dongdong-28 <louisp0215@gmail.com>
2023-01-10 15:02:32 +0900

    feate modify commit

--- src/main/java/index.java
+++ src/main/java/index.java
@@ -6,6 +6,6 @@ public class index {
         System.out.println("only dev");
         System.out.println("dev 2");
         System.out.println("new feat");
-        System.out.println("new feat3");
+        System.out.println("new feat2");
     }
 }
